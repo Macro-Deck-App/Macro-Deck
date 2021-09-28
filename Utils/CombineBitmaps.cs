eb48e382734be2f14e53032b24848fdc55ae68c5
Manuel Mayer <45011179+SuchByte@users.noreply.github.com>
2021-09-28 18:21:55 +0200

    Add files via upload

---
+++ Utils/CombineBitmaps.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Text;
+
+namespace SuchByte.MacroDeck.Utils
+{
+    public class CombineBitmaps
+    {
+        public static Bitmap CombineAll(List<Bitmap> bitmaps)
+        {
+            Bitmap combined = new Bitmap(350, 350);
+
+            using (Graphics g = Graphics.FromImage(combined))
+            {
+                g.SmoothingMode = SmoothingMode.AntiAlias;
+                foreach(Bitmap bitmap in bitmaps)
+                {
+                    g.DrawImage(bitmap, Point.Empty);
+                }
+            }
+
+            return combined;
+        }
+
+
+        public static Bitmap Combine(Bitmap backgroundBitmap, Bitmap iconBitmap)
+        {
+            int px = 350;
+            if (backgroundBitmap == null) backgroundBitmap = new Bitmap(px, px);
+            if (iconBitmap == null) iconBitmap = new Bitmap(px, px);
+            Image background = new Bitmap(backgroundBitmap.Width, backgroundBitmap.Height, PixelFormat.Format32bppArgb);
+            Image icon = new Bitmap(background.Width, background.Height, PixelFormat.Format32bppArgb);
+            using (var g = Graphics.FromImage(background))
+            {
+                g.Clear(Color.Transparent);
+            }
+            using (var g = Graphics.FromImage(icon))
+            {
+                g.Clear(Color.Transparent);
+            }
+
+
+            var bitmap = new Bitmap(px, px);
+            var canvas = Graphics.FromImage(bitmap);
+            canvas.InterpolationMode = InterpolationMode.Bicubic;
+            canvas.DrawImage(background,
+                             new Rectangle(0,
+                                           0,
+                                           background.Width,
+                                           background.Height),
+                             new Rectangle(0,
+                                           0,
+                                           background.Width,
+                                           background.Height),
+                             GraphicsUnit.Pixel);
+            canvas.DrawImage(icon,
+                             (bitmap.Width / 2) - (icon.Width / 2),
+                             (bitmap.Height / 2) - (icon.Height / 2));
+
+
+            canvas.Save();
+
+            return bitmap;
+        }
+    }
+}
---
+++ Utils/LabelGenerator.cs
@@ -0,0 +1,60 @@
+﻿using SuchByte.MacroDeck.ActionButton;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Text;
+
+namespace SuchByte.MacroDeck.Utils
+{
+    public class LabelGenerator
+    {
+        public static Bitmap GetLabel(Bitmap img, String text, ButtonLabelPosition buttonLabelPosition, Font font, Color textColor, Color shadowColor, SizeF shadowOffset)
+        {
+            if (img == null) return img;
+
+            Graphics g = Graphics.FromImage(img);
+
+            StringFormat sf = new StringFormat
+            {
+                Alignment = StringAlignment.Center
+            };
+
+            if (buttonLabelPosition == ButtonLabelPosition.TOP)
+                sf.LineAlignment = StringAlignment.Near;
+            else if (buttonLabelPosition == ButtonLabelPosition.CENTER)
+                sf.LineAlignment = StringAlignment.Center;
+            else
+                sf.LineAlignment = StringAlignment.Far;
+
+            Pen p = new Pen(Color.Black, 2)
+            {
+                LineJoin = LineJoin.Round
+            };
+
+            Rectangle fr = new Rectangle(0, img.Height - font.Height, img.Width, font.Height);
+            LinearGradientBrush b = new LinearGradientBrush(fr, textColor, textColor, 90);
+
+            Rectangle r = new Rectangle(2, 2, img.Width - 2, img.Height - 2);
+
+            GraphicsPath gp = new GraphicsPath();
+
+            gp.AddString(text, font.FontFamily, (int)font.Style, font.Size * 5, r, sf);
+
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+            g.DrawPath(p, gp);
+            g.FillPath(b, gp);
+
+            gp.Dispose();
+            b.Dispose();
+            b.Dispose();
+            font.Dispose();
+            sf.Dispose();
+            g.Dispose();
+
+            return img;
+        }
+    }
+}
