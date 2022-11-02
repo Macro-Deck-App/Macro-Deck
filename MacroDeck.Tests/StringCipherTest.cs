c1666afb6fcf9616d7032cdf3869d8375f365e66
RecklessBoon <16234384+RecklessBoon@users.noreply.github.com>
2022-11-02 16:48:27 -0500

    Sets up test suite, adds one test (#312)

---
+++ MacroDeck.Tests/StringCipherTest.cs
@@ -0,0 +1,25 @@
+using SuchByte.MacroDeck.Utils;
+
+namespace MacroDeck.Tests;
+
+public class StringCipherTests
+{
+    private string machineId;
+    
+    [SetUp]
+    public void Setup()
+    {
+        machineId = StringCipher.GetMachineGuid();
+    }
+
+    [TestCase("This is a short test")]
+    [TestCase("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")]
+    public void EncryptionTest(string unencrypted)
+    {
+        var encrypted = StringCipher.Encrypt(unencrypted, machineId);
+        Assert.That(encrypted, Is.Not.EqualTo(unencrypted));
+
+        var decrypted = StringCipher.Decrypt(encrypted, machineId);
+        Assert.That(decrypted, Is.EqualTo(unencrypted));
+    }
+}
